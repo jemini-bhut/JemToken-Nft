a1587f042e8b7188fe237abe0171f80dc65380fa
Jemini Bhut <58567102+jemini-bhut@users.noreply.github.com>
2022-05-26 12:22:05 +0530

    Add files via upload

---
+++ Token Code/JemTokenNft.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+
+/// @custom:security-contact jeminibhut@gmail.com
+contract JemToken is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public mintRate = 1;
+    uint256 public MAX_SUPPLY = 1000;
+
+    constructor() ERC721("JemToken", "JEM") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://JemTokens.com/Tokens";
+    }
+
+    function safeMint(address to) public payable {
+        require(totalSupply() < MAX_SUPPLY, "Not enought Ether sent.");
+        require(msg.value >= mintRate, "Not Enough Ether Sent");
+        _tokenIdCounter.increment();
+        _safeMint(to, _tokenIdCounter.current());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function withdraw() public onlyOwner{
+        require(address(this).balance > 0, "Balance is 0");
+        payable(owner()).transfer(address(this).balance);
+    }
+}
